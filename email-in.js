cc5f075e80009d0922b4b80b8c82b1f7e0761c8e
Jan Lehnardt <jan@apache.org>
2012-06-25 23:13:14 +0200

    refactor to proper module

--- email-in.js
+++ email-in.js
@@ -35,82 +35,126 @@ TODO:
  - tests
  - more docs
 */
+
 var http = require('http');
 var url = require("url");
 var request = require("request");
 
-var port = process.env["PORT"] || 8888;
+function WorkerEmailInPostmark(config) {
+    this._config = config;
 
+    // Variable to hold chunked data from Postmark.
+    var mailRaw = '';
+
+    http.createServer(function (req, res) {
+
+        req.on('data', function(chunk) {
+            mailRaw += chunk;
+        });
+
+        req.on('end', function() {
+
+            // Get the JSON payload from Postmark.
+            if(!mailRaw) { 
+                console.log("no mailRaw, skip");
+                res.end();
+                return;
+            }
+
+            var mailJSON = this._postMarkToHoodie(mailRaw);
+
+            // hoodie+hash@inbound.postmarkapp.com
+            var db = this._parseDbName(mailJSON);
+
+            this._doSaveDoc(db, mailJSON);
+
+            // Reset our holder variable.
+            mailRaw = '';
+
+            // Send an empty response.
+            res.end();
+            console.log("req-done yay");
+        });
+
+    }).listen(port, '0.0.0.0');
+
+    console.log('Server running at http://0.0.0.0:' + port);
+
+}
+
+WorkerEmailInPostmark.prototype._parseDbName = function(doc)
+{
+    // if "mail+hash@..."
+    //   db = hash
+    if(doc.MailboxHash) {
+        return doc.MailboxHash;
+    }
+
+    // if From == known user
+    //   db = user db
+    if(doc.From) {
+        // mangle via config wubble
+        return doc.From;
+    }
+
+    // else
+    //   db = catchall db for losers
+    return this._config.default_db;
+}
+
+WorkerEmailInPostmark.prototype._doSaveDoc = function(db, doc)
+{
+    // Insert new document.
+    var uri = url.parse(this._config.server);
+    uri.path = "/" + db + "/";
+    uri.auth = this._config.admin.user + ":" + this._config.admin.pass;
+    request({
+        uri: uri,
+        method: "POST",
+        json: doc
+    }, function(error, response) {
+        if(error) {
+            console.log("Set Doc status fail: " + error);
+        }
+        //  console.log("Save doc response: %j", response);
+    });
+
+}
+
+WorkerEmailInPostmark.prototype._postMarkToHoodie = function(mailRaw)
+{
+    var mailJSON = JSON.parse(mailRaw);
+
+    // Transform attachments (fairly cheesy, but seems to work).
+    if(mailJSON.Attachments.length) {
+        var couchAttachments = '{';
+        for(var i=0; i<mailJSON.Attachments.length; i++) {
+            couchAttachments += '"' + mailJSON.Attachments[i].Name + '": {';
+            couchAttachments += '"content_type":"' + mailJSON.Attachments[i].ContentType + '",';
+            couchAttachments += '"data":"' + mailJSON.Attachments[i].Content + '"';
+            couchAttachments += '},';
+        }
+        couchAttachments = couchAttachments.substring(0, (couchAttachments.length)-1);
+        couchAttachments += '}';
+        var _attachments = JSON.parse(couchAttachments);
+
+        // Replace old attachments property with new Couchified attachments.
+        mailJSON._attachments = _attachments;
+        delete mailJSON.Attachments;
+    }
+    return mailJSON;
+}
+
+// setup
+var port = process.env["PORT"] || 8888;
 var config = {
     server: process.env.HOODIE_SERVER,
     admin: {
         user: process.env.HOODIE_ADMIN_USER,
         pass: process.env.HOODIE_ADMIN_PASS
-    }
+    },
+    default_db: process.env.HOODIE_MAIL_IN_DEFAULT_DB || "hoodie-lone-mail"
 }
 
-// Variable to hold chunked data from Postmark.
-var mailRaw = '';
-
-http.createServer(function (req, res) {
-
-    req.on('data', function(chunk) {
-        mailRaw += chunk;
-    });
-
-    req.on('end', function() {
-
-        // Get the JSON payload from Postmark.
-        if(!mailRaw) { 
-            console.log("no mailRaw, skip");
-            res.end();
-            return;
-        }
-        var mailJSON = JSON.parse(mailRaw);
-
-        // Transform attachments (fairly cheesy, but seems to work).
-        if(mailJSON.Attachments.length) {
-            var couchAttachments = '{';
-            for(var i=0; i<mailJSON.Attachments.length; i++) {
-                couchAttachments += '"' + mailJSON.Attachments[i].Name + '": {';
-                couchAttachments += '"content_type":"' + mailJSON.Attachments[i].ContentType + '",';
-                couchAttachments += '"data":"' + mailJSON.Attachments[i].Content + '"';
-                couchAttachments += '},';
-            }
-            couchAttachments = couchAttachments.substring(0, (couchAttachments.length)-1);
-            couchAttachments += '}';
-            var _attachments = JSON.parse(couchAttachments);
-
-            // Replace old attachments property with new Couchified attachments.
-            mailJSON._attachments = _attachments;
-            delete mailJSON.Attachments;
-        }
-
-
-        // Insert new document.
-        var db = mailJSON.MailboxHash.toLowerCase(); // hoodie+hash@inbound.postmarkapp.com
-        var uri = url.parse(config.server);
-        uri.path = "/" + db + "/";
-        uri.auth = config.admin.user + ":" + config.admin.pass;
-        request({
-          uri: uri,
-          method: "POST",
-          json: mailJSON
-        }, function(error, response) {
-          if(error) {
-                console.log("Set Doc status fail: " + error);
-          }
-              //console.log("Save doc response: %j", response);
-        });
-
-        // Reset our holder variable.
-        mailRaw = '';
-
-        // Send an empty response.
-        res.end();
-        console.log("req-done yay");
-    });
-
-}).listen(port, '0.0.0.0');
-
-console.log('Server running at http://0.0.0.0:' + port);
+// let's go
+var worker = new WorkerEmailInPostmark(config);
